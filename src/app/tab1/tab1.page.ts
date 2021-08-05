cf4ecfcaeaf8a97cd0218fe15d0e710580ffbd75
Vinicius4002 <86272856+Vinicius4002@users.noreply.github.com>
2021-08-05 17:33:51 -0300

    Update tab1.page.ts

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +8,109 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController,public toastController: ToastController) {}
+  async curtir() {
+    const alert = await this.alertController.create({
+      header: 'Confirmação',
+      message: 'Tem certeza desta ação',
+      buttons: [
+        {
+          text: 'Não',
+          role: 'cancel',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Sim',
+          handler: () => {
+            this.xd();
+          }
+        }
+      ]
+    });
 
+    await alert.present();
+  }
+
+  async xd() {
+    const toast = await this.toastController.create({
+      message: 'Like atribuido',
+      duration: 2000,
+      position: 'middle',
+      color: 'tertiary'
+    });
+    toast.present();
+  }
+
+  async nota() {
+    const alert = await this.alertController.create({
+      header: 'Nota',
+      inputs: [
+        {
+          name: 'radio1',
+          type: 'radio',
+          label: '1',
+          value: 'value1',
+          handler: () => {
+            console.log('Radio 1 selected');
+          },
+          checked: true
+        },
+        {
+          name: 'radio2',
+          type: 'radio',
+          label: '2',
+          value: 'value2',
+          handler: () => {
+            console.log('Radio 2 selected');
+          }
+        },
+        {
+          name: 'radio3',
+          type: 'radio',
+          label: '3',
+          value: 'value3',
+          handler: () => {
+            console.log('Radio 3 selected');
+          }
+        },
+        {
+          name: 'radio4',
+          type: 'radio',
+          label: '4',
+          value: 'value4',
+          handler: () => {
+            console.log('Radio 4 selected');
+          }
+        },
+        {
+          name: 'radio5',
+          type: 'radio',
+          label: '5',
+          value: 'value5',
+          handler: () => {
+            console.log('Radio 5 selected');
+          }
+        },
+
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Votar!',
+          handler: () => {
+            console.log('Confirm Ok');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
